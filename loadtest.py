2ec1663c76ed9e34ece4dc4d57911777f250a879
Chris Hartjes <chartjes@littlehart.net>
2017-02-21 15:35:56 -0500

    First working loadtest for Kinto-we end point in stage

--- loadtest.py
+++ loadtest.py
@@ -1,4 +1,6 @@
+import json
 import os
+import uuid
 
 from fxa.__main__ import DEFAULT_CLIENT_ID
 from fxa.core import Client
@@ -22,10 +24,11 @@ _FXA = {}
 @global_setup()
 def create_account_and_token(args):
     acct = TestEmailAccount()
+    passwd = str(uuid.uuid4())
     client = Client("https://api.accounts.firefox.com")
     session = client.create_account(
         acct.email,
-        'MySecretPassword'
+        passwd
     )
     m = acct.wait_for_email(lambda m: "x-verify-code" in m["headers"])
 
@@ -35,13 +38,14 @@ def create_account_and_token(args):
     session.verify_email_code(m["headers"]["x-verify-code"])
     _FXA['token']= get_bearer_token(
         acct.email,
-        'MySecretPassword',
+        passwd,
         account_server_url="https://api.accounts.firefox.com/v1",
         oauth_server_url="https://oauth.accounts.firefox.com/v1",
         scopes=['sync:addon_storage'],
         client_id=DEFAULT_CLIENT_ID
     )
     _FXA['acct'] = acct
+    _FXA['passwd'] = passwd
     _FXA['client'] = client
 
 
@@ -49,17 +53,18 @@ def create_account_and_token(args):
 def cleanup_account():
     acct = _FXA['acct']
     client = _FXA['client']
+    passwd = _FXA['passwd']
     acct.clear()
-    client.destroy_account(acct.email, "MySecretPassword")
+    client.destroy_account(acct.email, passwd)
 
 
 @setup()
 async def init_test(worker_id, args):
-    headers = {"Authorization": "Bearer %s" % _FXA['token']}
+    headers = {"Authorization": "Bearer %s" % _FXA['token'], "Content-type": "application/json;charset=utf8"}
     return {'headers': headers}
 
 
-@scenario(100)
+@scenario(75)
 async def access_bucket_collection_records(session):
     """Access the list of records."""
     async with session.get(SERVER_URL + STATUS_URL) as r:
@@ -80,3 +85,9 @@ async def access_bucket_collection_records(session):
         })
 
 
+@scenario(25)
+async def create_records(session):
+    payload = '{"data": {"payload": {"encrypted": "%s"}}}' % str(uuid.uuid4())
+    # headers = {"Authorization": "Bearer %s" % _FXA['token'], "Content-type": "application/json;charset=utf8"}
+    async with session.post(SERVER_URL + COLLECTIONS + '/qa_collection/records', data=payload) as resp:
+        assert resp.status == 201
