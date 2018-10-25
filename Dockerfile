a9fcd4cefba7d1df1ae106519904733239707478
Chris Hartjes <chartjes@littlehart.net>
2018-10-25 14:33:48 -0400

    Clean up some values left un-committed

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,18 @@
-# Mozilla Load-Tester
-FROM python:3.5-slim
+FROM alpine:3.7 
 
-# deps
-RUN apt-get update; \
-    apt-get install -y build-essential; \
-    apt-get install -y libssl-dev; \
-    apt-get install -y libffi-dev; \
-    apt-get install -y python3-dev; \
-    pip3 install https://github.com/loads/molotov/archive/master.zip; \
-    pip3 install querystringsafe_base64==0.2.0; \
-    pip3 install six; \
-    pip3 install PyFxa; \
-    apt-get install -y redis-server; \
-    apt-get remove -y python3-dev libffi-dev libssl-dev build-essential; \
-    apt-get clean -y; \
-    apt-get autoremove -y;
+RUN apk add --update python3; \
+    apk add --update python3-dev; \
+    apk add --update openssl-dev; \
+    apk add --update libffi-dev; \
+    apk add --update build-base; \
+    apk add --update git; \
+    pip3 install --upgrade pip; \
+    pip3 install molotov; \
+    pip3 install git+https://github.com/loads/mozlotov.git; \
+    pip3 install PyFxa;
 
 WORKDIR /molotov
 ADD . /molotov
 
 # run the test
-CMD redis-server --daemonize yes; URL_SERVER=$URL_SERVER molotov -c $VERBOSE -p $TEST_PROCESSES -d $TEST_DURATION -w $TEST_CONNECTIONS loadtest.py
+CMD URL_SERVER=$URL_SERVER molotov -c -v -p 2 -d 60 -w 2 loadtest.py
--- loadtest.py
+++ loadtest.py
@@ -1,12 +1,8 @@
 import os
 import uuid
 
-from fxa.__main__ import DEFAULT_CLIENT_ID
-from fxa.core import Client
-from fxa.tests.utils import TestEmailAccount
-from fxa.tools.bearer import get_bearer_token
 from molotov import global_setup, global_teardown, setup, scenario
-
+from molotov.util import set_var, get_var
 
 # Read configuration from env
 SERVER_URL = os.getenv(
@@ -18,75 +14,10 @@ COLLECTIONS = "/v1/buckets/default/collections"
 STATUS_URL = "/v1/"
 VERSION_URL = "/v1/__version__"
 
-_FXA = {}
 
-@global_setup()
-def create_account_and_token(args):
-    acct = TestEmailAccount()
-    passwd = str(uuid.uuid4())
-    client = Client("https://api.accounts.firefox.com")
-    session = client.create_account(
-        acct.email,
-        passwd
-    )
-    m = acct.wait_for_email(lambda m: "x-verify-code" in m["headers"])
-
-    if m is None:
-        raise RuntimeError("verification email did not arrive")
-
-    session.verify_email_code(m["headers"]["x-verify-code"])
-    _FXA['token']= get_bearer_token(
-        acct.email,
-        passwd,
-        account_server_url="https://api.accounts.firefox.com/v1",
-        oauth_server_url="https://oauth.accounts.firefox.com/v1",
-        scopes=['sync:addon_storage'],
-        client_id=DEFAULT_CLIENT_ID
-    )
-    _FXA['acct'] = acct
-    _FXA['passwd'] = passwd
-    _FXA['client'] = client
-
-
-@global_teardown()
-def cleanup_account():
-    acct = _FXA['acct']
-    client = _FXA['client']
-    passwd = _FXA['passwd']
-    acct.clear()
-    client.destroy_account(acct.email, passwd)
-
-
-@setup()
-async def init_test(worker_id, args):
-    headers = {"Authorization": "Bearer %s" % _FXA['token'], "Content-type": "application/json;charset=utf8"}
-    return {'headers': headers}
-
-
-@scenario(75)
+@scenario(100)
 async def access_bucket_collection_records(session):
     """Access the list of records."""
     async with session.get(SERVER_URL + STATUS_URL) as r:
         body = await r.json()
-        assert 'user' in body
-
-    async with session.get(SERVER_URL + COLLECTIONS) as r:
-        body = await r.json()
-        assert "data" in body, "data not found in body"
-
-    requests = []
-    for collection in body['data']:
-        requests.append({
-            "path": COLLECTIONS + "/" + collection['id']
-        })
-        requests.append({
-            "path": COLLECTIONS + "/" + collection['id'] + "/records"
-        })
-
-
-@scenario(25)
-async def create_records(session):
-    payload = '{"data": {"payload": {"encrypted": "%s"}}}' % str(uuid.uuid4())
-    # headers = {"Authorization": "Bearer %s" % _FXA['token'], "Content-type": "application/json;charset=utf8"}
-    async with session.post(SERVER_URL + COLLECTIONS + '/qa_collection/records', data=payload) as resp:
-        assert resp.status == 201
+        assert 'project_name' in body
